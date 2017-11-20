a82edebd4729665eca5a2b283973b232746e2904
luca-venturi <luca.venturi.92@gmail.com>
2017-11-20 14:00:58 -0500

    haar done. ax.stem Error ?

--- hw4/time/regress.py
+++ hw4/time/regress.py
@@ -18,8 +18,34 @@ def get_data() :
     y = np.dot(X,_beta) + np.random.randn(n)
     return X[:num_train,:],y[:num_train],X[num_train:,:],y[num_train:]
 
+def squareLoss(x,y,b):
+	loss = 0.
+	for i in range(y.size):
+		loss += (np.dot(b,x[i,:]) - y[i]) ** 2
+	return loss
+
 def main() :
-    train_X,train_y,valid_X,valid_y = get_data()
-    
+	n = 5
+	train_X,train_y,valid_X,valid_y = get_data()
+	N = train_y.size	
+	M = np.zeros((n,n))
+	v = np.zeros((n))
+	for i in range(n):
+		M += np.outer(train_X[i,:],train_X[i,:])
+		v += train_y[i] * train_X[i,:]
+	# least square	
+	beta_est = np.linalg.solve(M,v)
+	print 'estimated beta: ', beta_est
+	print 'train loss: ', squareLoss(train_X,train_y,beta_est)
+	print 'test loss: ', squareLoss(valid_X,valid_y,beta_est)
+	_, s, _ = np.linalg.svd(train_X, full_matrices=False)
+	print 'singular values of training data: ', s
+	# ridge regression
+	beta_est = np.linalg.solve(M + 0.5 * np.eye(n),v)
+	print 'RR estimated beta: ', beta_est
+	print 'RR train loss: ', squareLoss(train_X,train_y,beta_est)
+	print 'RR test loss: ', squareLoss(valid_X,valid_y,beta_est)
+	
+
 if __name__ == "__main__" :
     main()
